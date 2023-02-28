bbb89e7e1963c33c3547b0e8b6dc3c6c2845687d
jomil <jomil@DESKTOP-19MT1VD>
2023-02-28 20:19:59 +0100

    El proyecto ya esta terminado.

--- src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/MainApp.java
@@ -1,13 +1,20 @@
 package org.iesalandalus.programacion.alquilervehiculos;
 
-import org.iesalandalus.programacion.alquilervehiculos.controlador.Controlador;
+
 import org.iesalandalus.programacion.alquilervehiculos.modelo.Modelo;
+import org.iesalandalus.programacion.alquilervehiculos.modelo.controlador.Controlador;
 import org.iesalandalus.programacion.alquilervehiculos.vista.Vista;
 
 public class MainApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// Ánimo!!!!
+		Vista vista = new Vista();
+		Modelo modelo = new Modelo();
+		Controlador controlador = new Controlador(modelo, vista);
+		vista.setControlador(controlador);
+		controlador.comenzar();
+		controlador.terminar();
 	}
 
 }
